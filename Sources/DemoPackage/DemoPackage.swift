d4d326cc2a7c5bb899dd681be55ba1bfa1a6ddad
Vieeveek Singh <vieeveek.singh@hotmail.com>
2024-01-23 16:59:29 +0530

    Swift Version Changes
    Swift Version Changes

--- Sources/DemoPackage/DemoPackage.swift
+++ Sources/DemoPackage/DemoPackage.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 public struct YourCustomView: View {
-    @available(macOS 10.15, *)
     public var body: some View {
         Text("Hello, Custom View!")
             .font(.title)
