8feff76138a9074237bdbcdd14624c3c8c6f38ca
Vieeveek Singh <vieeveek.singh@hotmail.com>
2024-01-23 17:03:48 +0530

    Change Class Name
    Change Class Name

--- Sources/DemoPackage/DemoPackage.swift
+++ Sources/DemoPackage/DemoPackage.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-public struct YourCustomView: View {
+public struct CustomView: View {
     public var body: some View {
         Text("Hello, Custom View!")
             .font(.title)
